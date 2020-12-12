2d16fdc643e80dd6438a1549b8f40e17a7a834bb
Perpaterb <zcarss@gmail.com>
2020-12-12 11:33:36 +1100

    rspec error asking for help

--- spec/Minesweeper_spec.rb
+++ spec/Minesweeper_spec.rb
@@ -1,8 +1,17 @@
 require_relative '../minesweeper_main_class'
 
 describe Minesweeper_Main do
-    it 'should return curect grid coordinates or invalid input' do
-        user_input = Minesweeper_Main.new("A1")
-        expect(user_input.processed).to eq("a1")
-    end
+    # should return true and curect grid coordinates or false
+    it do expect(Minesweeper_Main.new("A1").check_user_input).to eq([true, "a", 1]) end
+    it do expect(Minesweeper_Main.new("   D  4\n").check_user_input).to eq([true, "d", 4]) end
+    it do expect(Minesweeper_Main.new("  k  9").check_user_input).to eq([false]) end
+    it do expect(Minesweeper_Main.new("a29").check_user_input).to eq([false]) end
+end
+
+describe Minesweeper_Main do
+    # should return curect grid coordinates in numbers starting with row and then column
+    it do expect(Minesweeper_Main.new("A1").check_user_input.alfa_to_int_and_swap).to eq([1,1]) end
+    it do expect(Minesweeper_Main.new("   D  4\n").check_user_input.alfa_to_int_and_swap).to eq([4,4]) end
+    it do expect(Minesweeper_Main.new("  i  2").check_user_input.alfa_to_int_and_swap).to eq([2,9]) end
+    it do expect(Minesweeper_Main.new("F9").check_user_input.alfa_to_int_and_swap).to eq([9,6]) end
 end
